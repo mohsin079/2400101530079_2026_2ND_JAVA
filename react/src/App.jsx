73550d562418a3df61b0dcb9178c73bda9d9bf22
mohsin079 <mohsinmahfooz296@gmail.com>
2026-03-17 07:39:20 +0000

    jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -7,13 +7,22 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const name="Mohsin";
-  let 0;
-  const x=10;
+  let x=20;
+  let y=10;
+  function Name(){
+    return "Mohsin";
+    }
+
+  function sum(a,b){
+    return a+b;
+  }
   return (
     <div>
       <h1>Project run</h1>
       <h3>Name is {name}</h3>
-      <h3>{x+y}</h3>
+      <h3>{x}</h3>
+      <h4>{Name()}</h4>
+      <h2>parametrized {sum(10,40)}</h2>
     </div>
   )
 }
